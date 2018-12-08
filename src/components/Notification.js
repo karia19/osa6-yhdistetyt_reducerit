df4392e061ae8bb14f7ea814e62c54d94293c55e
karia19 <39730014+karia19@users.noreply.github.com>
2018-12-08 08:39:02 +0200

    Update Notification.js

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,51 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-class Notification extends React.Component {
-  componentDidMount() {
-    const { store } = this.context
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    )
-  }
-  render() {
+    const Message = ({ message }) => {
     
-    const Message = () => {
-      const mes = this.context.store.getState().notificationsKaksi
+      if (message === '' || message === undefined) {
+        return null
+      }
       
-  
-      if (mes === 'NEW')
-        return(
-          <p></p>
-        )
-      else {
         return (
            <div style={style}>
-            { this.context.store.getState().notificationsKaksi}
+            {message}
            </div>
         
         )
         
       }
-   
-    }
-    
     const style = {
       border: 'solid',
       padding: 10,
       borderWidth: 1
     }
-   
-  
-    return (
-      <Message />
-    )
-  }
-
-}
-
+    
 Notification.contextTypes = {
   store: PropTypes.object
 }
 
-export default Notification
+export default Message
